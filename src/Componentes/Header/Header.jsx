6433b72194e9fa96feb605acafaede3ee44995e6
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-03 10:03:56 -0300

    first commit

---
+++ src/Componentes/Header/Header.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../Navbar/Navbar";
+
+function Header() {
+    return ( 
+          
+        <header>
+            <span>Olá, visitante</span>
+            <h1>Bem vindo ao CineApp</h1>
+            <Navbar/>
+        </header>
+        
+     );
+}
+
+export default Header;
